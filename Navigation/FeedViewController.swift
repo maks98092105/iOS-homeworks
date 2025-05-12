b0684e32279d67de5535a39a683f81dfc0b9ac07
Maxim Kamyshev <maks.9809@list.ru>
2025-05-12 12:16:22 +0300

    creating a transition from FuuvVC to PostVC

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class FeedViewController: UIViewController {
 
+    private lazy var actionButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Перейти", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(actionButton)
 
-        // Do any additional setup after loading the view.
+        NSLayoutConstraint.activate([
+            actionButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            actionButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            actionButton.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            actionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        actionButton.addTarget(self, action: #selector(buttonPressed(_ :)), for: .touchUpInside)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func buttonPressed(_ sender: UIButton) {
+        let post = Post(title: "Первый пост")
+        let postVC = PostViewController(post: post)
+        
+        navigationController?.pushViewController(postVC, animated: true)
     }
-    */
-
 }
+
+
--- Navigation/Post.swift
+++ Navigation/Post.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct Post {
+    let title: String
+}
