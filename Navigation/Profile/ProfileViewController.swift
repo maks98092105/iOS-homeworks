e521b743f202fb24426b6ca9cf5d283da1a699e7
Maxim Kamyshev <maks.9809@list.ru>
2025-05-13 23:37:08 +0300

    create ProfileVC

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    private let profileHeader = ProfileHeaderView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .brown
+        
+        view.backgroundColor = .lightGray
+        view.addSubview(profileHeader)
     }
-
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        profileHeader.frame = view.bounds
+    }
 }
